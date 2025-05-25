8ac28b662f9742498377de91c2c0a972ac9a431e
Philip Cristobal <philipcristobal06@gmail.com>
2025-05-25 14:25:05 +0800

    Alternative flavor

---
+++ 6-alternative-flavor.js
@@ -0,0 +1,7 @@
+module.exports.items = ['item1', 'item2', 'item3', 'item4', 'item5'];
+
+const person = {
+  name: 'John'
+}
+
+module.exports.singlePerson = person;
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //modules
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
 
+console.log(data);
 sayHi(names.john);
 sayHi(names.peter);
 sayHi("susan");
