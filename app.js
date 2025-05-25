5e54a2b88cce18599e44ceab6f6dfaf7ef41ff23
Philip Cristobal <philipcristobal06@gmail.com>
2025-05-25 18:05:09 +0800

    mind grenade

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const names = require('./4-names');
 const sayHi = require('./5-utils');
 const data = require('./6-alternative-flavor');
 
-console.log(data);
-sayHi(names.john);
-sayHi(names.peter);
-sayHi("susan");
+// console.log(data);
+// sayHi(names.john);
+// sayHi(names.peter);
+// sayHi("susan");
 
+
+require('./7-mind-grenade');
